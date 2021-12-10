e7f0a6796f9a48d04f0225ade0cdf5f7a210e6bc
Fred Santiago <fredsantiago@wposs.com>
2021-12-10 15:33:55 -0500

    ajuste de visualizacion

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,35 +1,33 @@
-import React, { useEffect, useState } from "react";
-import Table from "../Table";
-import axios from 'axios';
+import React from "react";
 
-axios.defaults.adapter = require('axios/lib/adapters/http');
-
-const api = axios.create({
-  baseURL: 'https://reqres.in/api/users?page=2',
-});
-
-const listsUsers=async ()=>{
-    const {data}= await api.get('')
-    return data
-}
-
-
-async function Users()  {
-  const [todos, serTodos] = useState();
-
-  useEffect(()=>{
-    serTodos(listsUsers)
-  })
+export default function Users() {
+    const [users, setUsers] = React.useState([]);
+    const f = async () => {
+      const resp = await fetch("https://reqres.in/api/users/");
+      const json = await resp.json();
+      setUsers(json.data);
+    };
+    React.useEffect(() => {
+      f();
+    }, []);
+    return (
+      <div className="App">
+        <h1 className="center">Lista de usuarios</h1>
+        <div className="center">
+          {users.length &&
+            users.map((user) => {
+              return (
+                <div key={user.id}>
+                  <p>
+                    <strong>{user.first_name}</strong>
+                  </p>
+                  <p>{user.email}</p>
+                  <img src={user.avatar} />
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    );
+  }
   
-  
-  
-  return (
-    <div className="container">
-      {todos.map((todo, index) => {
-        return <Table characterData={todo} />;
-      })}
-    </div>
-  );
-}
-
-export default Users;
