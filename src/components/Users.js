6ed155f2c022a3b7d6f351c66374774cd8b4db4b
Fred Santiago <fredsantiago@wposs.com>
2021-12-11 13:19:29 -0500

    creacion de paginador

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,33 +1,53 @@
 import React from "react";
+import {FaAngleRight, FaAngleLeft} from "react-icons/fa";
+
 
 export default function Users() {
-    const [users, setUsers] = React.useState([]);
-    const aux = async () => {
-      const resp = await fetch("https://reqres.in/api/users?page = 2");
-      const json = await resp.json();
-      setUsers(json.data);
-    };
-    React.useEffect(() => {
-      aux();
-    }, []);
-    return (
-      <div className="App">
-        <h1>Lista de usuarios</h1>
-        <div className="flex">
-          {users.length &&
-            users.map((user) => {
-              return (
-                <div key={user.id} className="container-users">
-                  <p>
-                    <strong>{user.first_name} {user.last_name}</strong>
-                  </p>
-                  <img key={user.avatar} src={user.avatar} />
-                  <p>{user.email}</p>
-                </div>
-              );
-            })}
-        </div>
+ 
+  const [users, setUsers] = React.useState([]);
+  const [numPage, setNumPage] = React.useState([]);
+  const previous = async () => {
+    const resp = await fetch("https://reqres.in/api/users?page=" + 2);
+    const json = await resp.json();
+    setUsers(json.data);
+    setNumPage(json.page);
+  };
+
+  const next = async () => {
+    const resp = await fetch("https://reqres.in/api/users?page=" + 1);
+    const json = await resp.json();
+    setUsers(json.data);
+    setNumPage(json.page);
+  };
+  React.useEffect(() => {
+    previous();
+    next();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Lista de usuarios</h1>
+      <div className="center">
+        <button onClick={next}><FaAngleLeft></FaAngleLeft></button>
+        <span>{numPage}</span>
+        <button onClick={previous}><FaAngleRight></FaAngleRight></button>
       </div>
-    );
-  }
-  
+      <div className="flex">
+        {users.length &&
+          users.map((user) => {
+            return (
+              <div key={user.id} className="container-users">
+                <p>
+                  <strong>
+                    {user.first_name} {user.last_name}
+                  </strong>
+                </p>
+                <img key={user.avatar} src={user.avatar} />
+                <p>{user.email}</p>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
